c560cfac1e7e530071d41f71418eafc05a4a04d1
lolo <lucido.lorenzo@gmail.com>
2020-10-25 09:37:23 +0000

    Finalized home_service

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,40 +1,100 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <nav_msgs/Odometry.h>
+
+// Note the goals are slightly different than in pick_objects since we rotated the map by 180 degrees
+#define pickup_x -6.8   // -3.0
+#define pickup_y -3.0   // 6.8
+#define dropoff_x 8.4  // -5.4
+#define dropoff_y -5.4 // -8.4
+
+// Using a global variable for the state. Encapsulating it in a class would be a cleaner solution
+std::string state ("INIT"); // INIT, GO_PICK_OBJECT, GO_DELIVER_OBJECT, DONE
+visualization_msgs::Marker marker;
+ros::Publisher marker_pub;
+uint32_t shape = visualization_msgs::Marker::CUBE;
+
+float euclidean_dist(float x1, float y1, float x2, float y2) {
+  return sqrtf(pow(x2-x1, 2) + pow(y2-y1, 2));
+}
+
+bool close_to_ref(geometry_msgs::Pose pose, float ref_x, float ref_y, float min_dist = 0.35) {
+  float dist = euclidean_dist(pose.position.x, pose.position.y, ref_x, ref_y);
+  // ROS_INFO("DISTANCE from (%f, %f): %f (current: %f, %f)", ref_x, ref_y, dist, pose.position.x, pose.position.y);  
+  return dist < min_dist;
+}
+
+bool close_to_pickup(geometry_msgs::Pose pose) {  
+  return close_to_ref(pose, pickup_x, pickup_y); 
+}
+
+bool close_to_dropoff(geometry_msgs::Pose pose) {
+  return close_to_ref(pose, dropoff_x, dropoff_y); 
+}
+
+void odom_callback(const nav_msgs::Odometry::ConstPtr &msg) {
+  geometry_msgs::Pose current = msg->pose.pose;
+  
+  if(state == "INIT") {
+    ROS_INFO("Initialize: order to pickup object");
+    state = "GO_PICK_OBJECT";
+    marker.action = visualization_msgs::Marker::ADD; 
+     
+    marker.pose.position.x = pickup_y;
+    marker.pose.position.y = -pickup_x;
+    
+  }
+  else if(close_to_pickup(current) && state == "GO_PICK_OBJECT") {
+    ROS_INFO("Object picked up!");
+    state = "GO_DELIVER_OBJECT";
+    marker.action = visualization_msgs::Marker::DELETE;    
+    
+  }
+  else if(close_to_dropoff(current) && state == "GO_DELIVER_OBJECT") {
+    ROS_INFO("Object delivered!");
+    state = "DONE";
+    marker.pose.position.x = dropoff_y;
+    marker.pose.position.y = -dropoff_x;
+    marker.action = visualization_msgs::Marker::ADD;    
+    
+  };
+    
+  marker.header.stamp = ros::Time::now();  
+  marker_pub.publish(marker);
+}
 
 int main( int argc, char** argv )
 {
-  ros::init(argc, argv, "basic_shapes");
+  ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);       
+  ros::Subscriber odom_sub = n.subscribe("/odom", 1, &odom_callback);
 
   while (!ros::ok())
   {
     ROS_INFO("Waiting for ROS");
   }
-  visualization_msgs::Marker marker;
+  
   // Set the frame ID and timestamp.  See the TF tutorials for information on these.
   marker.header.frame_id = "map";
   marker.header.stamp = ros::Time::now();
 
   // Set the namespace and id for this marker.  This serves to create a unique ID
   // Any marker sent with the same namespace and id will overwrite the old one
-  marker.ns = "add_markers";
+  marker.ns = "basic_shapes";
   marker.id = 0;
 
   // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
   marker.type = shape;
 
   // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-  marker.action = visualization_msgs::Marker::ADD;
+  marker.action = visualization_msgs::Marker::DELETE;
 
   // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-  marker.pose.position.x = -3.0;
-  marker.pose.position.y = 6.8;
-  marker.pose.position.z = 0;
+  marker.pose.position.x = 0.0;
+  marker.pose.position.y = 0.0;
+  marker.pose.position.z = 0.0;
   marker.pose.orientation.x = 0.0;
   marker.pose.orientation.y = 0.0;
   marker.pose.orientation.z = 0.0;
@@ -51,55 +111,9 @@ int main( int argc, char** argv )
   marker.color.b = 0.0f;
   marker.color.a = 1.0;
 
-  // marker.lifetime = ros::Duration();
-
-  // Publish the marker
-  // while (marker_pub.getNumSubscribers() < 1)
-  // {
-  //   if (!ros::ok())
-  //   {
-  //     return 0;
-  //   }
-  //   ROS_WARN_ONCE("Please create a subscriber to the marker");
-  //   sleep(1);
-  // }
-
-  ROS_INFO("Preparing to send marker.");  
-  ros::Duration(5.0).sleep();
-
-  ROS_INFO("Publishing marker in pickup zone.");
+  // ROS_INFO("Preparing to send marker.");  
+  // ros::Duration(1.0).sleep();
   marker_pub.publish(marker);
-  ros::Duration(5.0).sleep();
-
-  marker.action = visualization_msgs::Marker::DELETE;
-  ROS_INFO("Object picked up");
-  marker_pub.publish(marker);
-  ros::Duration(5.0).sleep();
-
-  marker.action = visualization_msgs::Marker::ADD;
-  marker.pose.position.x = -5.4;
-  marker.pose.position.y = -8.4;
-  ROS_INFO("Publishing marker to dropoff zone");
-  marker_pub.publish(marker);
-
-  // Cycle between different shapes
-  // switch (shape)
-  // {
-  // case visualization_msgs::Marker::CUBE:
-  //   shape = visualization_msgs::Marker::SPHERE;
-  //   break;
-  // case visualization_msgs::Marker::SPHERE:
-  //   shape = visualization_msgs::Marker::ARROW;
-  //   break;
-  // case visualization_msgs::Marker::ARROW:
-  //   shape = visualization_msgs::Marker::CYLINDER;
-  //   break;
-  // case visualization_msgs::Marker::CYLINDER:
-  //   shape = visualization_msgs::Marker::CUBE;
-  //   break;
-  // }
-
-  r.sleep();
     
-  
+  ros::spin();
 }
--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -4,7 +4,8 @@ xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.
 sleep 11
 xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/RoboND-HomeServiceRobot/src/world/lolo_enhanced_map.yaml initial_pose_a:=-1.5707" &
 sleep 4
-# xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-xterm  -e  "source devel/setup.bash; rviz -d /home/workspace/RoboND-HomeServiceRobot/src/rvizConfig/rviz_cfg.rviz" &
-sleep 10
-xterm  -e  "source devel/setup.bash; rosrun add_markers add_markers_simple" &
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  "source devel/setup.bash; rosrun pick_objects pick_objects" &
+sleep 3
+xterm  -e  "source devel/setup.bash; rosrun add_markers add_markers" &
